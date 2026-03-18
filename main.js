133ed8ca49a3d38621e30be9d789b83924dc941d
Jessica9705 <jessica.fornari.097@gmail.com>
2026-03-18 22:06:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 let temp = prompt("Inserisci la temperatura esterna percepita:");
 
-if (temp < 20) {
-    console.log("Non ci sono più le mezze stagioni");
-} else if (temp >= 30) {
-    console.log("Lu mare, lu sole, lu ientu");
-} else if (temp < 30) {
-    console.log("mi dia una peroni sudata");
-} else if (temp < 0) {
-    console.log("non e’ tanto il freddo quanto l’umidità");
-} else if (temp < -10) {
-    console.log("copriti…ancora ti raffreddi");
+// if (temp < -10) {
+//     console.log("copriti...ancora ti raffreddi!");
+    
+// } else if (temp <= 0) {
+//     console.log("non è tanto il freddo quanto l'umidità");
+    
+// } else if (temp < 20) {
+//     console.log("non ci sono più le mezze stagioni");
+    
+// } else if (temp < 30) {
+//     console.log("mi dia una peroni sudata");
+    
+// } else {
+//     console.log("Lu mare, lu sole, lu ientu");
+    
+// }
+
+let messaggio;
+
+switch (true) {
+    case (temp < -10):
+        messaggio = "copriti...ancora ti raffreddi!";
+        break;
+    case (temp <= 0):
+        messaggio = "non è tanto il freddo quanto l'umidità";
+        break;
+    case (temp < 20):
+        messaggio = "non ci sono più le mezze stagioni";
+        break;
+    case (temp < 30):
+        messaggio = "mi dia una peroni sudata";
+        break;
+    default:
+        messaggio = "Lu mare, lu sole, lu ientu";
 }
+
+console.log(messaggio);
